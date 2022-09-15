7abf82468e6b2904e973b4b634c4886cb13d4952
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-09-14 22:31:35 -0500

    updated userInput Data and format

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 var generateBtn = document.querySelector("#generate");
 
 const userInput = {
-var special = ["£&@?!#%*+=€$_|<>-"];
-var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
-var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var numbers = ["0123456789"];
+characters: ['£','&','@', '?', '!', '#', '%', '*', '+', '=', '€', '$', '_', '|', '<', '>', '-']
+,lowerCase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+,upperCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+,numbers: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 }
 
 // Write password to the #password input
