cd0df68a179b70a8c4ba183a8406267cce5864ee
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-09-14 20:05:53 -0500

    added userInput data to JavaScript file

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+
+const userInput = {
+    lowerCase: "abcdefghijklmnopqrstuvwxyz",
+    upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numbers: "0123456789"
+    symbols: "`~!@#$%^&*()-_=+/?.>,<;:]}[{",
+};
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+
 
   passwordText.value = password;
   prompt()
