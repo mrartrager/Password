ea48387f1752667844897254a14aac14ec4ad1df
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-09-16 22:49:52 -0500

    everything logs to console now_rewrote for loop

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let lowerCase =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p",
 let upperCase = ["A", "B","C","D","E", "F","G", "H", "I","J","K", "L", "M","N","O","P", "Q", "R","S","T", "U", "V", "W", "X", "Y", "Z"];
 let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 let characters = ["£","&","@","?","!","#","%","*","+","=","€","$","_","|","<",">","-"];
-let all = [];
+
 
 // Write password to the #password input
 function writePassword() {
@@ -25,43 +25,54 @@ function writePassword() {
   let specials = confirm("Do you want your password to contain any special characters?");
 
 
-  let charSet = [];
+  let all = [];
   passwordText.value = password;
   
 
   if (lowerLetters) {
     console.log("Your password will have lower case!")
     all = all.concat(lowerCase)
+    password = lowerLetters
   } else {
-    console.log("Your password will not have lower case")
+    console.log("Your password will not have lower case, wow you want to get hacked?")
   }
 
   if (upperLetters) {
     console.log("Your password will have Upper case!")
     all = all.concat(upperCase)
-    
+    password = upperLetters
+  } else {
+    console.log ("Your password will not have Upper case, wow you want to get hacked?")
   }
   
   if (num) {
-    all = all.concat(characters)
-  
+    console.log("Your password will have numbers!")
+    all = all.concat(numbers)
+    password = num
+  } else {
+    console.log("Your password will not have numbers, wow you want to get hacked?")
   }
 
   if (specials) {
-    all = all.concat(lowerCase)
-    
+    console.log("Your password will have special characters!")
+    all = all.concat(characters)
+    password = specials
+  } else {
+    console.log("Your password will not have special characters, wow you want to get hacked?")
   }
 
   console.log(all);
-
-  // for (let i=0; i < passwordLength: i++) {
-  //   let 
-  // }
+  
 }
 
 
 function generatePassword () {
 
+for (let i = 0; i < password; i++){
+let password = all.Math.floor(Math.random() * password.value);
+return password.value;
+}
+ return password;
 }
 
 
