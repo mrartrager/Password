9e235af80352a632881277ad813e6ba8c1b12f5c
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-09-17 01:24:53 -0500

    tried some more for loop solutions_no luck.

--- script.js
+++ script.js
@@ -5,18 +5,16 @@ let lowerCase =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p",
 let upperCase = ["A", "B","C","D","E", "F","G", "H", "I","J","K", "L", "M","N","O","P", "Q", "R","S","T", "U", "V", "W", "X", "Y", "Z"];
 let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 let characters = ["£","&","@","?","!","#","%","*","+","=","€","$","_","|","<",">","-"];
-
+let randomPassword = [];
 
 // Write password to the #password input
 function writePassword() {
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
- 
   let passwordLength = prompt("Choose a number between 8 and 128 characters. This will be how long your password is.");
-
   if (passwordLength < 8 || passwordLength > 128) {alert("Hey dude, your password is not between 8 characters and 128 characters. Try again.");
   passwordLength = prompt("Choose a password length of at least 8 characters and no more than 128 characters.");
-  return null; 
+  
 }
 
   let lowerLetters = confirm("Do you want your password to contain any lowercase letters?");
@@ -25,56 +23,64 @@ function writePassword() {
   let specials = confirm("Do you want your password to contain any special characters?");
 
 
-  let all = [];
   passwordText.value = password;
   
 
   if (lowerLetters) {
     console.log("Your password will have lower case!")
-    all = all.concat(lowerCase)
-    password = lowerLetters
+    randomPassword = randomPassword.concat(lowerCase)
   } else {
-    console.log("Your password will not have lower case, wow you want to get hacked?")
+    console.log("Your password will NOT have lower case, wow, you want to get hacked?")
   }
 
   if (upperLetters) {
     console.log("Your password will have Upper case!")
-    all = all.concat(upperCase)
-    password = upperLetters
+    randomPassword = randomPassword.concat(upperCase)
   } else {
-    console.log ("Your password will not have Upper case, wow you want to get hacked?")
+    console.log ("Your password will NOT have Upper case, wow, you want to get hacked?")
   }
   
   if (num) {
     console.log("Your password will have numbers!")
-    all = all.concat(numbers)
-    password = num
+   randomPassword = randomPassword.concat(numbers)
   } else {
-    console.log("Your password will not have numbers, wow you want to get hacked?")
+    console.log("Your password will NOT have numbers, wow, you want to get hacked?")
   }
 
   if (specials) {
     console.log("Your password will have special characters!")
-    all = all.concat(characters)
-    password = specials
+    randomPassword = randomPassword.concat(characters)
   } else {
-    console.log("Your password will not have special characters, wow you want to get hacked?")
+    console.log("Your password will NOT have special characters, wow, you want to get hacked?")
   }
+  console.log(randomPassword);
 
-  console.log(all);
+  let word=""; 
+  for (let i = 0; i < passwordLength; i++){
+    word += randomPassword[Math.floor(Math.random()* randomPassword.length)]
+  }
+  return word 
+}
+
+generateBtn.addEventListener("click", writePassword);
+function generatePassword () {
+ 
   
 }
 
 
-function generatePassword () {
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
 
-for (let i = 0; i < password; i++){
-let password = all.Math.floor(Math.random() * password.value);
-return password.value;
-}
- return password;
-}
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
+//   passwordText.value = password;
+//   prompt()
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
